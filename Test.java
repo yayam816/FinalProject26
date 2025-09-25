3116bbda55e24a63c46f3a85d19eba93a4f75d8f
Zona-firman <zonafirman2@gmail.com>
2025-09-25 08:22:27 +0700

    Perubahan 27

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Ini adalah baris yang saya buat -26");
+        System.err.println("ini Adalah perubahan saya -27");
     }
 }
