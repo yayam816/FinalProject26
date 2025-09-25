8c32d5dbfe458d75883ac132520d637a22c7b7db
yayam816 <yasya160806@gmail.com>
2025-09-25 08:39:07 +0700

    undo2

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Ini adalah baris yang saya buat -26");
-        System.err.println("Perubahan ini dibatalkan");
+        System.err.println("Perubahan ini saya gagalkannnnnnnn");
     }
 }
